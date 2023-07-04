8eded98a67940b8ebd507a94403c5ecb48491f03
Artem <artemm3note@gmail.com>
2023-07-05 01:38:41 +0300

    Made animation for labels, now it work correct (with dirty fix)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { labelUp } from './label-up.js'
+import { labelUp, labelDown } from './label-up.js'
 import './styles/main.scss'
 import logo from './assets/img/logo.jpg'
 
 document.querySelector('#logo').src = logo
 
 labelUp()
+labelDown()
